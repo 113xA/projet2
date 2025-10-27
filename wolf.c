f71e8ca5c6dd19eed95e8d4f421bb0bf3d312193
raphpirard <raphpirard>
2025-10-27 15:30:08 +0100

    modified rabbit and wolf

--- wolf.c
+++ wolf.c
@@ -9,7 +9,33 @@
 
 #include "wolf.h"
 
+Action wolfFindAction (Animal *wolf, Grid *grid, Position position)
+{
+
+}
+
+Animal *reproduceWolf(Animal *wolf)
+{
+    int wolfEnergy = animalGetEnergy(wolf);
+
+    if(wolfEnergy >= wolfReproduceThreshold)
+    {
+        Animal *newWolf;
+        newWolf = wolfCreate();
+        animalLoseEnergy(wolf);
+        return newWolf;
+    }
+
+    else
+        return NULL;
+}
+
+
+
+
 Animal *wolfCreate(void)
 {
-    return NULL;
+    Animal *NewWolf = animalCreate(wolfName, wolfPriority, wolfInitEnergy, wolfActionEnergy, wolfEatEnergy, 
+                                  wolfFindAction, reproduceWolf);
+    return NewWolf;
 }
