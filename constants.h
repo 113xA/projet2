8b19161140940e54ec499ddc108b5baac29fc162
113xA <alex.113xA@gmail.com>
2025-10-27 18:06:41 +0100

    Fixed constants.h

--- constants.h
+++ constants.h
@@ -2,52 +2,52 @@
 #define CONSTANTS_H
 
 // The names of the animals
-const char *rabbitName;
-const char *wolfName;
+extern const char *rabbitName;
+extern const char *wolfName;
 
 // The priorities of the animals, the lower the faster.
-const unsigned int wolfPriority;
-const unsigned int rabbitPriority;
+extern const unsigned int wolfPriority;
+extern const unsigned int rabbitPriority;
 
 // The maximum distances the animals can see. Used when computing the scores.
-const int rabbitMaxDistance;
-const int wolfMaxDistance;
+extern const int rabbitMaxDistance;
+extern const int wolfMaxDistance;
 
 // Energy parameters related to the rabbit.
 
 // Energy when created.
-const int rabbitInitEnergy;
+extern const int rabbitInitEnergy;
 // Energy consumed when an action is performed.
-const int rabbitActionEnergy;
+extern const int rabbitActionEnergy;
 // Energy gained when grass is eated.
-const int rabbitEatEnergy;
+extern const int rabbitEatEnergy;
 // Energy lost when the rabbit reproduces.
-const int rabbitReproduceEnergy;
+extern const int rabbitReproduceEnergy;
 // Energy threshold required for reproduction.
-const int rabbitReproduceThreshold;
+extern const int rabbitReproduceThreshold;
 
 // Energy parameters related to the wolf.
 
 // Energy when created.
-const int wolfInitEnergy;
+extern const int wolfInitEnergy;
 // Energy consumed when an action is performed.
-const int wolfActionEnergy;
+extern const int wolfActionEnergy;
 // Energy gained when grass is eated.
-const int wolfEatEnergy;
+extern const int wolfEatEnergy;
 // Energy lost when the wolf reproduces.
-const int wolfReproduceEnergy;
+extern const int wolfReproduceEnergy;
 // Energy threshold required for reproduction.
-const int wolfReproduceThreshold;
+extern const int wolfReproduceThreshold;
 
 // Weights used when computing the score of the rabbit.
-const int rabbitGrassWeight;
-const int rabbitWolfWeight;
+extern const int rabbitGrassWeight;
+extern const int rabbitWolfWeight;
 
 // Probabilites used when making appear grass.
 // The probability that some grass will appear on an empty cell is:
 // P = grassProb + N * grassIncProb, where N is the number of adjacent cells with grass
 // (0 <= N <= 4)
-const double grassProb;
-const double grassIncProb;
+extern const double grassProb;
+extern const double grassIncProb;
 
 #endif
